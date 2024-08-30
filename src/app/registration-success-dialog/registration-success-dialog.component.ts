60d42b322fd9aceb0328ab5a42241d6ea8c701ad
Nazir12B <nazirgbadamassi24@gmail.com>
2024-08-30 13:18:16 +0200

    fix: update responsive

---
+++ src/app/registration-success-dialog/registration-success-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject  } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+@Component({
+  selector: 'app-registration-success-dialog',
+  imports: [ CommonModule,],
+  standalone: true,
+  templateUrl: './registration-success-dialog.component.html',
+  styleUrls: ['./registration-success-dialog.component.scss']
+})
+export class RegistrationSuccessDialogComponent {
+  isSuccess: boolean;
+  apiMessage: string;
+
+
+  constructor(
+    public dialogRef: MatDialogRef<RegistrationSuccessDialogComponent>,  // Injecter MatDialogRef
+    @Inject(MAT_DIALOG_DATA) public data: { message: string, isSuccess: boolean },
+    private router: Router
+  ) {
+    this.isSuccess = data.isSuccess;
+    this.apiMessage = data.message;
+  }
+
+  onOkClick(): void {
+    console.log(this.apiMessage)
+    if (this.apiMessage == 'User created successfully.') {
+      this.dialogRef.close(); // Fermer le popup
+      this.router.navigate(['/']); // Redirige vers la page d'accueil ou une autre page
+    } else {
+      this.dialogRef.close(); // Fermer simplement le popup, reste sur la page de registre
+    }
+  }
+}
