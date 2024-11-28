1607b32312d1408ee97f0a9c0cb69d07f2b3f93f
Bintousarr <camarabintousarr@gmail.com>
2024-11-28 12:28:56 +0100

    Update slide

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,103 +1,55 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
-import { CompteAReboursComponent } from "../compte-a-rebours/compte-a-rebours.component";
-import { KeynoteSpeakerComponent } from "../keynote-speaker/keynote-speaker.component";
-import { SponsorComponent } from '../sponsor/sponsor.component';
-import { FooterComponent } from '../shared/footer/footer.component';
-import { EvenementComponent } from "../web/evenement/evenement.component";
-import { RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
-//import { TranslateModule, TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { EvenementComponent } from "../web/evenement/evenement.component";
+import { KeynoteSpeakerComponent } from "../keynote-speaker/keynote-speaker.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [TranslateModule, CompteAReboursComponent, KeynoteSpeakerComponent, SponsorComponent, EvenementComponent, RouterLink],
+  imports: [TranslateModule, EvenementComponent, KeynoteSpeakerComponent, CommonModule],
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
 })
-export class BannerComponent implements OnInit {
-  slides: any[] = [];
+export class BannerComponent implements OnInit, OnDestroy {
+  slides = [
+    { image: '../../../assets/head.png', title: 'accueil.banner-block.desc2', subtitle: 'accueil.banner-block.desc3', highlight: 'accueil.banner-block.desc4' },
+    { image: '../../../assets/slider.png', title: 'accueil.banner-block.desc2', subtitle: 'accueil.banner-block.desc3', highlight: 'accueil.banner-block.desc4' },
+  ];
   currentIndex = 0;
   slideInterval: Subscription | undefined;
-  translate: TranslateService = inject(TranslateService)
-
-
+  translate: TranslateService = inject(TranslateService);
 
   ngOnInit() {
-    // this.translate.setDefaultLang('en');
     this.translate.setDefaultLang('en');
 
-    
-    // Utilise onLangChange pour s'assurer que les traductions sont chargées et initialisées
-    // this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-    //   this.initializeSlides();
-    // });
-
-
-    // Appel initial pour initialiser les slides à la première charge
-    // this.initializeSlides();
-
-    // Commence le diaporama
-    //this.startSlideShow();
+    this.startSlideShow();
   }
 
-      
-  translateText(lang: string) {
-    this.translate.use(lang);
-  }
-
-  initializeSlides() {
-    this.translate.get([
-      'accueil.banner-block.conference',
-      'accueil.banner-block.date',
-      'accueil.banner-block.year',
-      'accueil.banner-block.description',
-      'accueil.banner-block.about',
-      'accueil.banner-block.register',
-      'accueil.banner-block.desc1'
-    ]).subscribe(translations => {
-      this.slides = [
-        {
-          conference: translations['accueil.banner-block.conference'],
-          date: translations['accueil.banner-block.date'],
-          year: translations['accueil.banner-block.year'],
-          description: translations['accueil.banner-block.description'],
-          aboutText: translations['accueil.banner-block.about'],
-          registerText: translations['accueil.banner-block.register'],
-          desc1: translations['accueil.banner-block.desc1'],
-          image: '../../../assets/Slider1.jpg'
-        }/* ,
-        {
-          conference: translations['accueil.banner-block.desc1'],
-          year: translations['accueil.banner-block.year'],
-          description: translations['accueil.banner-block.description'],
-          aboutText: translations['accueil.banner-block.about'],
-          registerText: translations['accueil.banner-block.register'],
-          image: '../../../assets/IMG_2388.png'
-        } */
-      ];
-    });
-  }
-
- /*  ngOnDestroy() {
-    if (this.slideInterval) {
-      this.slideInterval.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.stopSlideShow();
   }
 
   startSlideShow() {
-    this.slideInterval = interval(3000).subscribe(() => {
+    this.slideInterval = interval(5000).subscribe(() => {
       this.nextSlide();
     });
   }
 
-  prevSlide() {
-    this.currentIndex = (this.currentIndex === 0) ? this.slides.length - 1 : this.currentIndex - 1;
+  stopSlideShow() {
+    this.slideInterval?.unsubscribe();
   }
 
   nextSlide() {
-    this.currentIndex = (this.currentIndex === this.slides.length - 1) ? 0 : this.currentIndex + 1;
-  } */
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  translateText(lang: string) {
+    this.translate.use(lang);
+  }
 }
