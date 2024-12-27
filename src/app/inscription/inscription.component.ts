8ea88db6beeeac60ab6de37ce93889d9baf5e409
Nazir12B <nazirgbadamassi24@gmail.com>
2024-12-27 10:19:35 +0100

    fix: correction

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [CommonModule,FormsModule, TranslateModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule, ReactiveFormsModule],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.scss'
 })
@@ -22,7 +22,7 @@ export class InscriptionComponent {
   ];
   total = 0;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.paymentForm = this.fb.group({
       product0: [false],
       product1: [false],
@@ -37,19 +37,43 @@ export class InscriptionComponent {
   }
 
   ngOnInit(): void {
+    const productIndex = Number(this.route.snapshot.queryParamMap.get('productIndex'));
+  
+    const controlsConfig: any = {};
+    this.products.forEach((_, index) => {
+      controlsConfig[`product${index}`] = [index === productIndex]; // Par défaut, coche le produit sélectionné
+    });
+  
+    this.paymentForm = this.fb.group({
+      ...controlsConfig,
+      paymentMethod: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  
+    this.calculateTotal();
+  
     this.paymentForm.valueChanges.subscribe(() => {
       this.calculateTotal();
     });
-    this.translate.setDefaultLang('en');
-
   }
-
+  
+  onCheckboxChange(selectedIndex: number): void {
+    this.products.forEach((_, index) => {
+      const controlName = `product${index}`;
+      this.paymentForm.get(controlName)?.setValue(index === selectedIndex);
+    });
+  
+    this.calculateTotal();
+  }
+  
   calculateTotal(): void {
     this.total = this.products.reduce((acc, product, index) => {
       return acc + (this.paymentForm.get(`product${index}`)?.value ? product.price : 0);
     }, 0);
   }
-
+  
   onSubmit(): void {
     if (this.paymentForm.valid) {
       console.log('Form Data:', this.paymentForm.value);
@@ -60,9 +84,9 @@ export class InscriptionComponent {
   }
   translate: TranslateService = inject(TranslateService)
 
-  
+
   translateText(lang: string) {
     this.translate.use(lang);
   }
-  
+
 }
